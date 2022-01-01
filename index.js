6d5c13a99dac9454ba5b48d47b6f5bc20c610a09
KainatSana <88139861+KainatSana@users.noreply.github.com>
2022-01-01 23:50:03 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const path=require('path');
 require('express-async-errors');
 const winston=require('winston');
 require('winston-mongodb');
